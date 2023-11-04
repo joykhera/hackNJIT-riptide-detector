a64f9de6561a228718f7e8bc23d663c25c2a2d4d
joykhera <joykhera@gmail.com>
2023-11-04 18:43:03 -0400

    make get request take in beach

--- src/app/tideData/[station]/route.ts
+++ src/app/tideData/[station]/route.ts
@@ -1,16 +1,15 @@
 import { NextResponse } from "next/server";
-import beaches from './beaches.json';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { beaches } from '../beaches';
+
+console.log('keyof typeof beaches;', )
+type BeachNameType = keyof typeof beaches;
 
 interface DataPoint {
     t: string;
     v: number; // Assuming after parsing, 'v' will always be a number
 }
 
-interface Beaches {
-    [key: string]: string;
-}
-
-const beachesTyped: Beaches = beaches;
 
 const NOAA_API_LINK = 'https://api.tidesandcurrents.noaa.gov/api/prod/datagetter';
 
@@ -21,26 +20,21 @@ const apiOptions = {
     units: 'english',
     time_zone: 'lst_ldt',
     datum: 'STND',
-    station: beachesTyped['San Francisco, CA']
 }
 
 function calculateRateOfChange(data: DataPoint[]): number[] {
-    // Convert string values to numbers for calculation
     data.forEach((point: DataPoint) => {
-        // Ensure 'v' is treated as a string here, even though it's a union type
         if (typeof point.v === 'string') {
             point.v = parseFloat(point.v);
         }
     });
 
-    // Calculate the absolute change in water level between each minute
+
     const changes = data.map((point, index, array) => {
-        if (index === 0) return 0; // No change at the first data point
-        // Since we've already converted all 'v' values to numbers, this subtraction should be fine
+        if (index === 0) return 0;
         return Math.abs(point.v - array[index - 1].v);
     });
 
-    // Remove the first element (0) as it's not a change
     changes.shift();
 
     return changes;
@@ -49,32 +43,23 @@ function calculateRateOfChange(data: DataPoint[]): number[] {
 
 // Function to estimate the probability of rip currents
 function estimateRipCurrentProbability(changes: number[]): number {
-    // Define a threshold for significant change
-    const significantChangeThreshold = 0.02; // Example threshold
-
-    // Count how many changes are above the threshold
+    const significantChangeThreshold = 0.02;
     const significantChanges = changes.filter(change => change > significantChangeThreshold).length;
-
-    // Calculate the probability as the ratio of significant changes to total changes
     const probability = significantChanges / changes.length;
-
     return probability;
 }
 
-// To handle a GET request to /api
-export async function GET(request: Request): Promise<NextResponse> {
-    const queryParams = new URLSearchParams(apiOptions as any).toString();
-    const apiUrl = `${NOAA_API_LINK}?${queryParams}`;
+export async function GET(req: NextApiRequest, res: NextApiResponse) {
+    const stationName = req.url!.split('/')[4] as BeachNameType;
+    const queryParams = new URLSearchParams(apiOptions).toString();
+    const station = beaches[stationName]
+    const apiUrl = `${NOAA_API_LINK}?${queryParams}&station=${station}`;
     // console.log(apiUrl);
     const response = await fetch(apiUrl);
     const responseData = await response.json();
     // console.log(responseData);
 
-    // Assuming responseData.data is an array of DataPoint
-    // You would need to validate this and handle any discrepancies
     const changes = calculateRateOfChange(responseData.data as DataPoint[]);
-
-    // Estimate the probability of a rip current
     const probability = estimateRipCurrentProbability(changes);
     // console.log(probability);
 
---
+++ src/app/tideData/beaches.ts
@@ -0,0 +1,13 @@
+export const beaches = {
+    "san_francisco_ca": "9414290",
+    "the_Battery_ny": "8518750",
+    "rehoboth_beach_md": "8557863",
+    "key_west_fl": "8724580",
+    "providence_visibility": "8453662",
+    "cape_henry_ports_station": "cb0102",
+    "southport_nc": "CFR1624",
+    "eastport_estes_head": "EPT0003",
+    "grays_harbor_entrance_wa": "PCT1291",
+    "sewells_point_va": "8638610",
+    "buffalo_ny": "9063020"
+} as const;
