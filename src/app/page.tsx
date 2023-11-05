12e08d29b405158174685fa45b3c923b81e68713
joykhera <joykhera@gmail.com>
2023-11-04 21:45:08 -0400

    Autocomplete search bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
 import Navbar from "./components/navbar";
+import SearchBar from "./components/SearchBar";
 
 export default function Home() {
 	return (
 		<main>
 			<div className="h-20 bg-header-blue">
-				<Navbar />
+        <Navbar />
+        <SearchBar />
 			</div>
 			<div className="w-full h-screen bg-gray-200">
-				<div className="w-full h-full pt-6"></div>
+        <div className="w-full h-full pt-6"></div>
 			</div>
 		</main>
 	);
