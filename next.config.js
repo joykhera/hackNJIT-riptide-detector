be244a438f0de94f27e3822d9656d99bdb283e22
joykhera <joykhera@gmail.com>
2023-11-05 11:34:12 -0500

    change next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        WINDY_API_KEY: process.env.WINDY_API_KEY,
+    }
+}
 
 module.exports = nextConfig
